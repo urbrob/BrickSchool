5871d2d3a5f3d31df31c438b70b017b52c38deb3
gold7g <gold.ula4@gmail.com>
2019-09-27 21:22:01 +0200

    Merge

--- brickschool/schools/models.py
+++ brickschool/schools/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+	name = models.TextField(max_length=250)
+	city = models.TextField(max_length=250)
+	type_school = models.CharField(max_length=10)
+	patron = models.CharField(max_length=100)
+	website = models.URLField(max_length=200)
+	regon = models.CharField(max_length=14)
+	location = models.TextField(max_length=250)
+
+
 class Properties(models.Model):
     type_properties = models.CharField(max_length=10)
     complexity = models.CharField(max_length=255)
@@ -10,3 +20,4 @@ class Properties(models.Model):
     specjalization = models.CharField(max_length=255)
     staff_amount = models.IntegerField()
     school = models.ForeignKey(School, on_delete=models.CASCADE)
+	
