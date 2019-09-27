9e2f4c3e87d12715b1a7ba79cda4c48711c58cde
DawidCwiek <cwiekdawid1@gmail.com>
2019-09-27 21:33:50 +0200

    init feature/models

---
+++ app/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect
+} from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+import NotFound from "./views/404"
+import Home from "./views/Home"
+
+
+export const client = new ApolloClient({
+    uri: "http://localhost:8000/graphql"
+});
+
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/404" component={NotFound}/>
+                    <Redirect to="/404"/>
+                </Switch>
+            </Router>
+        </ApolloProvider>
+    );
+}
+
+export default App;
--- brickschool/schools/models.py
+++ brickschool/schools/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
+<<<<<<< HEAD
 # Create your models here.
+=======
+class School(models.Model):
+	name = models.TextField(max_length=250)
+	city = models.TextField(max_length=250)
+	type_school = models.CharField(max_length=10)
+	patron = models.CharField(max_length=100)
+	website = models.URLField(max_length=200)
+	regon = models.CharField(max_length=14)
+	location = models.TextField(max_length=250)
+
+
+>>>>>>> e3dede7f903e268d06d5a239cca971785d80625c
 class Properties(models.Model):
     type_properties = models.CharField(max_length=10)
     complexity = models.CharField(max_length=255)
@@ -10,3 +23,7 @@ class Properties(models.Model):
     specjalization = models.CharField(max_length=255)
     staff_amount = models.IntegerField()
     school = models.ForeignKey(School, on_delete=models.CASCADE)
+<<<<<<< HEAD
+=======
+	
+>>>>>>> e3dede7f903e268d06d5a239cca971785d80625c
