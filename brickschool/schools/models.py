652f68c8b42b71f8f810cd7583afaf271a984d68
DawidCwiek <cwiekdawid1@gmail.com>
2019-09-27 21:04:29 +0200

    add properties

--- brickschool/schools/models.py
+++ brickschool/schools/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Properties(models.Model):
+    type_properties = models.CharField(max_length=10)
+    complexity = models.CharField(max_length=255)
+    size_class = models.IntegerField()
+    publicity = models.CharField(max_length=255)
+    students_cathegory = models.CharField(max_length=100)
+    specjalization = models.CharField(max_length=255)
+    staff_amount = models.IntegerField()
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
