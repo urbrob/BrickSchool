10ec0f7515f92f2b12961772ed05990318075bc6
Robert <r.urbaniak@procsea.com>
2019-09-28 22:11:56 +0200

    apply filters

--- brickschool/schools/schema.py
+++ brickschool/schools/schema.py
@@ -1,9 +1,28 @@
 import graphene
 from .models import School, SchoolClass, Statistics, FinalExam
 from graphene_django.types import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 import django_filters
 from graphene import relay
 
+
+class SchoolFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='startswitch')
+    location = django_filters.CharFilter(lookup_expr='icontains')
+    perspective_badge = django_filters.ChoiceFilter(method='perspective_badge_filter', choices=['gold', 'silver', 'bronze'])
+    wsk = django_filters.NumberFilter(field_name='statistics__perspective_badge', lookup_expr='gt')
+
+    class Meta:
+        model = School
+        fields = ['is_public', 'type_school']
+
+    def perspective_badge_filter(self, queryset, name, value):
+        if value == 'gold':
+            return queryset.filter(statistics__perspective_badge__global_rating__lt=100)
+        elif value == 'silver':
+            return queryset.filter(statistics__perspective_badge__global_rating__lt=200)
+        elif value == 'bronze':
+            return queryset.filter(statistics__perspective_badge__global_rating__lt=500)
 
 class SchoolNode(DjangoObjectType):
     class Meta:
@@ -27,22 +46,12 @@ class FinalExamNode(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    schools = graphene.List(
+    schools = DjangoFilterConnectionField(
         SchoolNode,
-        first=graphene.Int(),
-        skip=graphene.Int(),
+        filterset_class=SchoolFilter
     )
     schools_list = graphene.List(graphene.String, school_name=graphene.Argument(graphene.String, required=False))
     school_detail = graphene.Field(SchoolNode, id=graphene.Argument(graphene.Int, required=True))
-
-
-    def resolve_schools(self, info, first=None, skip=None, **kwargs):
-        schools = School.objects.all()
-        if skip:
-            schools = schools[skip:]
-        if first:
-            schools = schools[:first]
-        return schools
 
     def resolve_schools_list(self, info, school_name=None):
         if school_name:
