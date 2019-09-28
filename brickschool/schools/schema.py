124c873cdc2df3e4235f8c990bf9491cf3862fef
Robert <r.urbaniak@procsea.com>
2019-09-28 21:21:20 +0200

    Add school detail query and filters

--- brickschool/schools/schema.py
+++ brickschool/schools/schema.py
@@ -5,77 +5,26 @@ import django_filters
 from graphene import relay
 
 
-class SchoolFilter(django_filters.FilterSet):
-    #name = django_filters.CharFilter(method=filter_cs)
-    class Meta:
-        model = School
-        fields = ['name',
-                'city',
-                'type_school',
-                'patron',
-                'website',
-                'regon',
-                'voivodship',
-                'county',
-                'community',
-                'postal_code',
-                'location',
-                ]
-
-
 class SchoolNode(DjangoObjectType):
     class Meta:
         model = School
-        filterset_class = SchoolFilter
         interfaces = (relay.Node, )
-
-
-class SchoolClassFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_type=['icontains'])
-    fields = ['specialization',
-            'school',
-            ]
 
 
 class SchoolClassNode(DjangoObjectType):
     class Meta:
         model = SchoolClass
-        filterset_class = SchoolClassFilter
-
-
-class StatisticsClassFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_type=['icontains'])
-    fields = ['students',
-            'girls',
-            'boys',
-            'school',
-            'year',
-            'staff_amount',
-            'sport_ranking',
-            ]
 
 
 class StatisticsNode(DjangoObjectType):
     class Meta:
         model = Statistics
-        filterset_class = StatisticsClassFilter
-
-
-class FinalExamFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_type=['icontains'])
-    fields = ['subject',
-            'avg_rate',
-            'pass_rate',
-            'is_extended',
-            'number_of_people',
-            'statistic',
-            ]
 
 
 class FinalExamNode(DjangoObjectType):
     class Meta:
         model = FinalExam
-        filterset_class = [FinalExamFilter]
+
 
 class Query(graphene.ObjectType):
     schools = graphene.List(
@@ -84,6 +33,7 @@ class Query(graphene.ObjectType):
         skip=graphene.Int(),
     )
     schools_list = graphene.List(graphene.String, school_name=graphene.Argument(graphene.String, required=False))
+    school_detail = graphene.Field(SchoolNode, id=graphene.Argument(graphene.Int, required=True))
 
 
     def resolve_schools(self, info, first=None, skip=None, **kwargs):
@@ -98,3 +48,6 @@ class Query(graphene.ObjectType):
         if school_name:
             return School.objects.filter(name__startswith=school_name)[:10].values_list('name', flat=True)
         return []
+
+    def resolve_school_detail(self, info, id):
+        return School.objects.get(id=id)
