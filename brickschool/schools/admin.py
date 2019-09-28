21c9e8962747961933cb747e5c259679c22ff2b1
gold7g <gold.ula4@gmail.com>
2019-09-28 09:38:14 +0200

    Created admin.py classes

--- brickschool/schools/admin.py
+++ brickschool/schools/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import School, SchoolClass, Statistics, final_exam, Badges
 
 # Register your models here.
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(SchoolClass)
+class SchoolClassAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Statistics)
+class StatisticsAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(FinalExam)
+class final_examAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Badges)
+class BadgesAdmin(admin.ModelAdmin):
+	pass
+
