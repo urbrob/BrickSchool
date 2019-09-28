03294e9ccbd2cd1a7bda39c0b271e13e96199710
DawidCwiek <cwiekdawid1@gmail.com>
2019-09-28 11:43:21 +0200

    add pagination

--- brickschool/schools/schema.py
+++ brickschool/schools/schema.py
@@ -7,10 +7,20 @@ class SchoolNode(DjangoObjectType):
         model = School
 
 
-
 class Query(graphene.ObjectType):
-    schools = graphene.List(SchoolNode)
+    schools = graphene.List(
+        SchoolNode,
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    )
 
 
-    def resolve_schools(self, info, **kwargs):
-        return School.objects.all()
+    def resolve_schools(self, info, search=None, first=None, skip=None, **kwargs):
+        schools = School.objects.all()
+
+        if skip:
+            schools = schools[skip:]
+        if first:
+            schools = schools[:first]
+
+        return schools
