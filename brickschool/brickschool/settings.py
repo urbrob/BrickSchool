ccb5bd4bbbf75c5fc42b631967d9e0f4a50471e6
DawidCwiek <cwiekdawid1@gmail.com>
2019-09-28 19:20:24 +0200

    fix graphene

--- brickschool/brickschool/settings.py
+++ brickschool/brickschool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(5dmuc@@g-y@3hfsv2f-lw6lkh2z&#xpc29h4$emkmie(n^*5y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'graphene_django',
     'schools',
     'quiz',
+    'corsheaders'
 ]
 
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'brickschool.urls'
