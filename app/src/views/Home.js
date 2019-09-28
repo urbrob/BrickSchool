960a6703e1a18fbde7f2c25f0bd9d793f46596e3
Graleszczur <Grales76@onet.pl>
2019-09-28 09:18:04 +0200

    resolve conflict

--- app/src/views/Home.js
+++ app/src/views/Home.js
@@ -1,14 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
-import { Layout, Menu } from "antd";
-import logo from "../images/logo.png";
-import StartingCard from "../components/home/StartingCard"
-=======
 import { Layout } from "antd";
 import background from "../images/bg1.jpg";
 import HeaderMenu from "../components/HeaderMenu";
 import FooterCredits from "../components/FooterCredits";
->>>>>>> 0f9c4fbdb1390f59029543b31365cd1ce0f347d2
+
 
 const { Content } = Layout;
 
@@ -27,7 +22,6 @@ const HomePage = () => {
             backgroundColor: "red"
           }}
         >
-          <StartingCard/>
 
         </Content>
         <FooterCredits />
