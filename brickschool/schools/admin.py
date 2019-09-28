b76920462f7c4006761edf45b30b1f243fafbdd3
Urszula Sołogub <34507753+urszulasologub@users.noreply.github.com>
2019-09-28 11:36:45 +0200

    Created admin.py classes (#24)
    * Created admin.py classes
    
    * Fixed final_exam to FinalExam

--- brickschool/schools/admin.py
+++ brickschool/schools/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import School, SchoolClass, Statistics, FinalExam, Badges
 
 # Register your models here.
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(SchoolClass)
+class SchoolClassAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Statistics)
+class StatisticsAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(FinalExam)
+class FinalExamAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Badges)
+class BadgesAdmin(admin.ModelAdmin):
+	pass
+
