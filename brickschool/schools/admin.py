6b773ea55551f1e87cc8b6cd452c509e9b2afca9
gold7g <gold.ula4@gmail.com>
2019-09-28 11:33:29 +0200

    small fixes

--- brickschool/schools/admin.py
+++ brickschool/schools/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import School, SchoolClass, Statistics, final_exam, Badges
+from .models import School, SchoolClass, Statistics, FinalExam, Badges
 
 # Register your models here.
 
--- brickschool/schools/schema.py
+++ brickschool/schools/schema.py
@@ -1,11 +1,31 @@
 import graphene
-from schools.models import School
+from .models import School, SchoolClass, Statistics, FinalExam, Badges
 from graphene_django.types import DjangoObjectType
+
 
 class SchoolNode(DjangoObjectType):
     class Meta:
         model = School
 
+
+class SchoolClassNode(DjangoObjectType):
+	class Meta:
+		model = SchoolClass
+
+
+class StatisticsNode(DjangoObjectType):
+	class Meta:
+		model = Statistics 
+	
+
+class FinalExamNode(DjangoObjectType):
+	class Meta:
+		model = FinalExam 
+
+
+class BadgesNode(DjangoObjectType):
+	class Meta:
+		model = Badges
 
 
 class Query(graphene.ObjectType):
