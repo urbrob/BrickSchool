cf9819d6194e64d6af27af95e0d661b0ecd69b4d
Robert <r.urbaniak@procsea.com>
2019-09-28 09:24:23 +0200

    Fix models

--- brickschool/schools/models.py
+++ brickschool/schools/models.py
@@ -8,52 +8,37 @@ class School(models.Model):
 	patron = models.CharField(max_length=100)
 	website = models.URLField(max_length=200)
 	regon = models.CharField(max_length=14)
-	location = models.TextField(max_length=250)
-
-
-class Properties(models.Model):
-    type_properties = models.CharField(max_length=10)
-    complexity = models.CharField(max_length=255)
-    size_class = models.IntegerField()
-    publicity = models.CharField(max_length=255)
-    students_cathegory = models.CharField(max_length=100)
-    specjalization = models.CharField(max_length=255)
-    staff_amount = models.IntegerField()
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    voivodship = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=100, null=True)
+    street = models.CharField(max_length=250, null=True)
+    house_number = models.CharField(max_length=50, null=True)
+    county = models.CharField(max_length=250, null=True)
+    community = models.CharField(max_length=250, null=True)
+    postal_code = models.CharField(max_length=6, null=True)
 
 
 class SchoolClass(models.Model):
-    specjalization = models.CharField(max_length=255)
+    specjalization = models.CharField(max_length=255, null=True)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
 
 
 class Statistics(models.Model):
-    students = models.IntegerField()
-    girls = models.IntegerField()
-    boys = models.IntegerField()
-    school = models.CharField(max_length=14)
-    year = models.IntegerField()
+    students = models.IntegerField(null=True)
+    girls = models.IntegerField(null=True)
+    boys = models.IntegerField(null=True)
+    school = models.ForeignKey(School)
+    year = models.DateField(null=True)
+	staff_amount = models.IntegerField()
     sport_ranking = models.IntegerField(default=0)
 
 
-class final_exam(models.Model):
-    subject = models.CharField(max_length=100)
-    extended = models.BooleanField()
-    avg_rate = models.IntegerField()
-    pass_rate = models.IntegerField()
-    statistic = models.IntegerField()
-    number_of_ppl = models.IntegerField()
-    statistic = models.ForeignKey(Statistics, on_delete=models.CASCADE)
-
-
-class Location(models.Model):
-    voivodship = models.CharField(max_length=50)
-    city = models.CharField(max_length=100)
-    street = models.CharField(max_length=250)
-    house_number = models.CharField(max_length=50)
-    county = models.CharField(max_length=250)
-    community = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=6)
+class FinalExam(models.Model):
+	subject = models.CharField(max_length=100)
+	avg_rate = models.IntegerField(null=True)
+	pass_rate = models.IntegerField(null=True)
+	is_extended = models.BooleanField(default=False)
+	number_of_people = models.IntegerField(null=True)
+	statistic = models.ForeignKey(Statistics, on_delete=models.CASCADE)
 
 
 class Badges(models.Model):
