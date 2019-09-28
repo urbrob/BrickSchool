494dc140044258cdfcf6a34312623c2f72f1a5fb
Michał Konieczka <m.konieczka12@gmail.com>
2019-09-29 00:53:26 +0200

    fetching list of schools (#36)

--- app/src/components/Record/RecordsList.js
+++ app/src/components/Record/RecordsList.js
@@ -5,85 +5,37 @@ import school_placeholder1 from "../../images/school_placeholder1.jpg";
 import school_placeholder2 from "../../images/school_placeholder2.jpg";
 import school_placeholder3 from "../../images/school_placeholder3.jpg";
 import Record from ".";
+import useSchoolsList from "../../hooks/useSchoolsList";
 
-const items = [
-  {
-    name: "Liceum Ogólnozniekształcające nr 5 w Pcimiu Dolnym",
-    location: "Pcim Dolny, dolnośląskie",
-    wsk: 50,
-    ranking: 4,
-    perspektywyBadge: "silver",
-    schoolType: "Liceum ogólnokształcące",
-    thumbnail: school_placeholder1
-  },
-  {
-    name: "Technikum nr 2 na Ślunsku",
-    location: "Wrocław, dolnośląskie",
-    wsk: 23,
-    ranking: 12,
-    perspektywyBadge: "bronze",
-    schoolType: "Technikum",
-    thumbnail: school_placeholder2
-  },
-  {
-    name: "Liceum Ogólnozniekształcające nr 12 w Łodzi",
-    location: "Uć, łódzkie",
-    wsk: 90,
-    ranking: 2,
-    perspektywyBadge: "gold",
-    schoolType: "Liceum ogólnokształcące",
-    thumbnail: school_placeholder3
-  },
-  {
-    name: "Liceum Ogólnozniekształcające nr 5 w Pcimiu Dolnym",
-    location: "Pcim Dolny, dolnośląskie",
-    wsk: 50,
-    ranking: 4,
-    perspektywyBadge: "silver",
-    schoolType: "Liceum ogólnokształcące",
-    thumbnail: school_placeholder1
-  },
-  {
-    name: "Technikum nr 2 na Ślunsku",
-    location: "Wrocław, dolnośląskie",
-    wsk: 23,
-    ranking: 12,
-    perspektywyBadge: "bronze",
-    schoolType: "Technikum",
-    thumbnail: school_placeholder2
-  },
-  {
-    name: "Liceum Ogólnozniekształcające nr 12 w Łodzi",
-    location: "Uć, łódzkie",
-    wsk: 90,
-    ranking: 2,
-    perspektywyBadge: "gold",
-    schoolType: "Liceum ogólnokształcące",
-    thumbnail: school_placeholder3
-  }
-];
+const RecordsList = () => {
+  const [schools, loading] = useSchoolsList(20);
 
-const getRecord = index => {
+  const getRecord = index => {
+    const formattedLocation = `${schools[index].location} ${schools[index].city}, ${schools[index].voivodship}`;
+
+    return (
+      <Record
+        name={schools[index].name}
+        schoolId={schools[index].id}
+        location={formattedLocation}
+        thumbnail={school_placeholder1}
+        schoolType={schools[index].typeSchool}
+        perspektywyBadge="bronze"
+        wsk={66}
+        ranking={1}
+      />
+    );
+  };
+
+  console.log("schools, loading: ", schools, loading);
   return (
-    <Record
-      name={items[index].name}
-      location={items[index].location}
-      thumbnail={items[index].thumbnail}
-      schoolType={items[index].schoolType}
-      perspektywyBadge={items[index].perspektywyBadge}
-      wsk={items[index].wsk}
-      ranking={items[index].ranking}
+    <Virtuoso
+      totalCount={schools.length}
+      overscan={10}
+      item={getRecord}
+      style={{ height: "100%", width: "100%" }}
     />
   );
 };
-
-const RecordsList = () => (
-  <Virtuoso
-    totalCount={items.length}
-    overscan={10}
-    item={getRecord}
-    style={{ height: "100%", width: "100%" }}
-  />
-);
 
 export default RecordsList;
