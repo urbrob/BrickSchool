9baebb4ea1d142dc06b0895b93a9af372dabcd7d
DawidCwiek <cwiekdawid1@gmail.com>
2019-09-28 11:10:45 +0200

    some test

--- brickschool/schools/schema.py
+++ brickschool/schools/schema.py
@@ -1,6 +1,16 @@
 import graphene
-
+from schools.models import School
 from graphene_django.types import DjangoObjectType
 
+class SchoolNode(DjangoObjectType):
+    class Meta:
+        model = School
+
+
+
 class Query(graphene.ObjectType):
-    pass
+    schools = graphene.List(SchoolNode)
+
+
+    def resolve_schools(self, info, **kwargs):
+        return School.objects.all()
