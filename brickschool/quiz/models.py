c5b6a667e8afc6932bbccd6dd8425b6a63b86222
gold7g <gold.ula4@gmail.com>
2019-09-28 18:44:36 +0200

    Changed models in quiz: now there's a list of cathegories

--- brickschool/quiz/models.py
+++ brickschool/quiz/models.py
@@ -6,6 +6,16 @@ class Quiz(models.Model):
 
 
 class Question(models.Model):
+    CATEGORIES_CHOICES = (
+        ('0', 'Inteligencja logiczno-matematyczna'),
+        ('1', 'Inteligencja lingwistyczna'),
+        ('2', 'Inteligencja muzyczna'),
+        ('3', 'Inteligencja interpersonalna'),
+        ('4', 'Inteligencja intrapersonalna'),
+        ('5', 'Inteligencja fizyczno-kinestetyczna'),
+        ('6', 'Inteligencja przyrodnicza'),
+        ('7', 'Inteligencja wizualno-przestrzenna'),
+    )
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, choices=CATEGORIES_CHOICES, default='0')
