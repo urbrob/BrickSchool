7c5eaf0fb941bf2f0f3189fbaf914ad17ab1ba8d
Robert <r.urbaniak@procsea.com>
2019-09-29 10:08:02 +0200

    Add pk

--- brickschool/schools/schema.py
+++ brickschool/schools/schema.py
@@ -42,9 +42,14 @@ class PerspectiveBadge(DjangoObjectType):
 
 
 class SchoolNode(DjangoObjectType):
+    pk = graphene.Field(graphene.Int)
+
     class Meta:
         model = School
         interfaces = (relay.Node, )
+
+    def resolve_id(self, info):
+        return self.id
 
 
 class SchoolClassNode(DjangoObjectType):
@@ -68,7 +73,7 @@ class Query(graphene.ObjectType):
         filterset_class=SchoolFilter
     )
     schools_list = graphene.List(graphene.String, school_name=graphene.Argument(graphene.String, required=False))
-    school_detail = graphene.Field(SchoolNode, id=graphene.Argument(graphene.Int, required=True))
+    school_detail = graphene.Field(SchoolNode, pk=graphene.Argument(graphene.Int, required=True))
 
     def resolve_schools_list(self, info, school_name=None):
         if school_name:
@@ -76,5 +81,5 @@ class Query(graphene.ObjectType):
             return School.objects.filter(name__contains=school_name)[:10].values_list('name', flat=True)
         return []
 
-    def resolve_school_detail(self, info, id):
-        return School.objects.get(id=id)
+    def resolve_school_detail(self, info, pk):
+        return School.objects.get(id=pk)
